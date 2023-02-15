20b2270009348e50eeef50569a3108d7194da762
Malak-Enany <malakenan140@gmail.com>
2023-02-15 09:34:40 +0200

    edit responsive

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const menubt = document.getElementById("menu-bt");
 const closebt = document.getElementById("close");
+const peopleimg = document.getElementById("people-img");
 const menu = document.getElementById("menu");
 
-
-menubt.onclick = function () { showmenu() };
-function showmenu()
-{
+menubt.onclick = function () {
+  showmenu();
+};
+function showmenu() {
   menu.style.left = 0;
+  document.getElementById("people-img").style.display = "none";
 }
 
 closebt.onclick = function () {
   closemenu();
 };
-function closemenu()
-{
+function closemenu() {
   menu.style.left = "-200%";
+  document.getElementById("people-img").style.display = "flex";
 }
